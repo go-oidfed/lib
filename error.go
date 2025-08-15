996f20aadc83181056b28b9840d74e9589a6df1d
zachmann <gabriel.zachmann@kit.edu>
2025-08-15 10:39:57 +0200

    [error handling] return pointer for all error constructors

--- error.go
+++ error.go
@@ -22,88 +22,88 @@ const (
 )
 
 // ErrorInvalidRequest returns an Error for using InvalidRequest
-func ErrorInvalidRequest(description string) Error {
-	return Error{
+func ErrorInvalidRequest(description string) *Error {
+	return &Error{
 		Error:            InvalidRequest,
 		ErrorDescription: description,
 	}
 }
 
 // ErrorInvalidClient returns an Error for using InvalidClient
-func ErrorInvalidClient(description string) Error {
-	return Error{
+func ErrorInvalidClient(description string) *Error {
+	return &Error{
 		Error:            InvalidClient,
 		ErrorDescription: description,
 	}
 }
 
 // ErrorInvalidIssuer returns an Error for using InvalidIssuer
-func ErrorInvalidIssuer(description string) Error {
-	return Error{
+func ErrorInvalidIssuer(description string) *Error {
+	return &Error{
 		Error:            InvalidIssuer,
 		ErrorDescription: description,
 	}
 }
 
 // ErrorInvalidSubject returns an Error for using InvalidSubject
-func ErrorInvalidSubject(description string) Error {
-	return Error{
+func ErrorInvalidSubject(description string) *Error {
+	return &Error{
 		Error:            InvalidSubject,
 		ErrorDescription: description,
 	}
 }
 
 // ErrorInvalidTrustAnchor returns an Error for using InvalidTrustAnchor
-func ErrorInvalidTrustAnchor(description string) Error {
-	return Error{
+func ErrorInvalidTrustAnchor(description string) *Error {
+	return &Error{
 		Error:            InvalidTrustAnchor,
 		ErrorDescription: description,
 	}
 }
 
 // ErrorInvalidTrustChain returns an Error for using InvalidTrustChain
-func ErrorInvalidTrustChain(description string) Error {
-	return Error{
+func ErrorInvalidTrustChain(description string) *Error {
+	return &Error{
 		Error:            InvalidTrustChain,
 		ErrorDescription: description,
 	}
 }
 
 // ErrorInvalidMetadata returns an Error for using InvalidMetadata
-func ErrorInvalidMetadata(description string) Error {
-	return Error{
+func ErrorInvalidMetadata(description string) *Error {
+	return &Error{
 		Error:            InvalidMetadata,
 		ErrorDescription: description,
 	}
 }
 
 // ErrorNotFound returns an Error for using NotFound
-func ErrorNotFound(description string) Error {
-	return Error{
+func ErrorNotFound(description string) *Error {
+	return &Error{
 		Error:            NotFound,
 		ErrorDescription: description,
 	}
 }
 
 // ErrorServerError returns an Error for using ServerError
-func ErrorServerError(description string) Error {
-	return Error{
+func ErrorServerError(description string) *Error {
+	return &Error{
 		Error:            ServerError,
 		ErrorDescription: description,
 	}
 }
 
 // ErrorUnsupportedParameter returns an Error for using UnsupportedParameter
-func ErrorUnsupportedParameter(description string) Error {
-	return Error{
+func ErrorUnsupportedParameter(description string) *Error {
+	return &Error{
 		Error:            UnsupportedParameter,
 		ErrorDescription: description,
 	}
 }
 
 // ErrorTemporarilyUnavailable returns an Error for using TemporarilyUnavailable
-func ErrorTemporarilyUnavailable(description string) Error {
-	return Error{
+func ErrorTemporarilyUnavailable(description string) *Error {
+	return &Error{
 		Error:            TemporarilyUnavailable,
 		ErrorDescription: description,
 	}
